45dc5b74f06f6705e0eebd7e834e732143d74fc9
mgrom-dev <kkdvkg@ya.ru>
2019-12-26 10:25:24 +0700

    load Project CustomerList

---
+++ src/CustomerStorage.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+public class CustomerStorage
+{
+    private HashMap<String, Customer> storage;
+
+    public CustomerStorage()
+    {
+        storage = new HashMap<>();
+    }
+
+    public void addCustomer(String data)
+    {
+        String[] components = data.split("\\s+");
+        String name = components[0] + " " + components[1];
+        storage.put(name, new Customer(name, components[3], components[2]));
+    }
+
+    public void listCustomers()
+    {
+        storage.values().forEach(System.out::println);
+    }
+
+    public void removeCustomer(String name)
+    {
+        storage.remove(name);
+    }
+
+    public int getCount()
+    {
+        return storage.size();
+    }
+}
