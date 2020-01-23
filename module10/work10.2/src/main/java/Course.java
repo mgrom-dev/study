8b458ef2569d57630bc9bb9fe6dee1744f1e6377
mgrom-dev <kkdvkg@ya.ru>
2020-01-24 01:04:26 +0700

    adding use lombok to work 10.2

--- module10/work10.2/src/main/java/Course.java
+++ module10/work10.2/src/main/java/Course.java
@@ -1,7 +1,10 @@
+import lombok.Data;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "Courses")
+@Data
 public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,76 +30,4 @@ public class Course {
 
     @Column(name = "price_per_hour")
     private float pricePerHour;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-
-    public CourseType getType() {
-        return type;
-    }
-
-    public void setType(CourseType type) {
-        this.type = type;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getTeacherId() {
-        return teacherId;
-    }
-
-    public void setTeacherId(int teacher_id) {
-        this.teacherId = teacher_id;
-    }
-
-    public int getStudentsCount() {
-        return studentsCount;
-    }
-
-    public void setStudentsCount(int students_count) {
-        this.studentsCount = students_count;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public float getPricePerHour() {
-        return pricePerHour;
-    }
-
-    public void setPricePerHour(float price_per_hour) {
-        this.pricePerHour = price_per_hour;
-    }
 }
--- module10/work10.2/src/main/java/Teacher.java
+++ module10/work10.2/src/main/java/Teacher.java
@@ -1,7 +1,10 @@
+import lombok.Data;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "Teachers")
+@Data
 public class Teacher {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,36 +15,4 @@ public class Teacher {
     private int salary;
 
     private int age;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getSalary() {
-        return salary;
-    }
-
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
