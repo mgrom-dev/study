655186e43eaeec571490d0ba3971617a33742daa
mgrom-dev <kkdvkg@ya.ru>
2020-02-10 13:51:37 +0700

    add limit to max active Threads work 11.3

--- module11/work11.3/src/main/java/GetPage.java
+++ module11/work11.3/src/main/java/GetPage.java
@@ -5,44 +5,21 @@ import org.jsoup.nodes.Element;
 
 import java.io.IOException;
 import java.net.Proxy;
+import java.util.ArrayList;
 import java.util.concurrent.RecursiveAction;
 
 public class GetPage extends RecursiveAction {
     private final String URL; //адрес сайта
-    private final Proxy PROXY; //прокси
-    private static SiteMap siteMap; //класс карты сайта, для сохранения результатов
 
-    public void setSiteMap(SiteMap siteMap){
-        GetPage.siteMap = siteMap;
-    }
-
-    public GetPage(String url, Proxy proxy) {
+    public GetPage(String url) {
         this.URL = url;
-        this.PROXY = proxy;
     }
 
     @Override
     protected void compute() {
-        try {
-            Document doc = Jsoup.connect(URL).proxy(PROXY).maxBodySize(0).get();
-            for (Element link : doc.select("a[href~=^" + siteMap.getUrl() + "|^/[a-z]]")){
-                String cutLink = link.attributes().get("href");
-                String fullLink = cutLink.matches("^" + siteMap.getUrl() + ".*") ? cutLink : siteMap.getUrl() + cutLink;
-                fullLink += fullLink.matches(".+/$") ? "" : "/";
-                if (siteMap.addChildPage(fullLink)){
-                    System.out.println(Thread.currentThread().getName() + ", найдена новая ссылка: " + fullLink);
-                    GetPage task = new GetPage(fullLink, PROXY);
-                    task.fork(); // запустим асинхронно
-                    task.join();
-                }
-            }
-        } catch (HttpStatusException httpex){
-            System.out.println("Page " + URL + " error. " + httpex);
-            if (httpex.getStatusCode() == 503) {
-                this.invoke();
-            }
-        } catch (IOException iex) {
-            System.out.println("Page " + URL + " not found. " + iex.getMessage());
-        }
+        Main.countThreads.incrementAndGet();
+        ArrayList<String> newUrls = Main.parsePage(this.URL);
+        Main.countThreads.decrementAndGet();
+        newUrls.forEach(Main::mapPageFork);
     }
 }
--- module11/work11.3/src/main/java/Main.java
+++ module11/work11.3/src/main/java/Main.java
@@ -2,30 +2,35 @@ import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.w3c.dom.ls.LSOutput;
 
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
+import java.util.ArrayList;
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
-    public final static String URL = "https://www.avnsk.com/"; //адрес сайта, который нужно пропарсить
+    public final static String URL = "https://www.avnsk.com"; //адрес сайта, который нужно пропарсить
     public final static String PATH = "src/main/data/site.txt"; //путь к файлу для сохранения карты сайта
     public final static boolean USE_PROXY = true; //использовать прокси для доступа в интернет
     public final static Proxy PROXY = USE_PROXY ? new Proxy(Proxy.Type.HTTP, new InetSocketAddress("192.168.230.198", 3128)) : Proxy.NO_PROXY; //свойства прокси
+    public final static int MAX_THREADS = 3; //максимальное количество парралельных потоков, которые будут парсить сайт на ссылки
+    public static AtomicInteger countThreads = new AtomicInteger(1); //текущее количество парралельных потоков
+    private final static ThreadGroup THREAD_GROUP = new ThreadGroup("get_url"); //группа для создания новых потоков
+    private final static String METHOD = "Threads"; //метод составления карты сайта
     private static SiteMap siteMap = new SiteMap(URL); //класс карты сайта
-    private static ThreadGroup threadGroup = new ThreadGroup("get_url"); //группа для создания новых потоков
-    private static String method = "Threads"; //метод составления карты сайта
 
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
 
-        if ("Threads".equals(method)) {
+        if ("Threads".equals(METHOD)) {
             //поиск с помощью паралельных потоков, где каждая найденная новая ссылка с сайта запускает новый паралельный поток для парсинга
             //почему-то данный метод работает быстрее, чем с ForkJoinPool
             mapPageThread(URL);
-            while(threadGroup.activeCount() > 0) {
+            while(THREAD_GROUP.activeCount() > 0) {
                 Thread.onSpinWait();
             }
         } else {
@@ -44,44 +49,60 @@ public class Main {
         }
     }
 
-
     /**
      * Поиск ссылок с помощью ForkJoinPool
      */
     public static void mapPageFork(String url){
-        GetPage getPage = new GetPage(url, PROXY);
-        getPage.setSiteMap(siteMap);
+        while (countThreads.get() > MAX_THREADS) {
+            Thread.onSpinWait();
+        }
+        GetPage getPage = new GetPage(url);
         new ForkJoinPool().invoke(getPage);
     }
 
     /**
-     * Поиск ссылок с помощью кучи параллельных потоков
+     * Поиск ссылок с помощью обычных потоков
      */
     public static void mapPageThread(String url){
-        //создаем новый паралельный поток
-        new Thread(threadGroup, ()->{
-            try {
-                Document doc = Jsoup.connect(url).proxy(PROXY).maxBodySize(0).get();
-                //получаем все ссылки которые ссылаются на адрес парсируемого сайта
-                for (Element link : doc.select("a[href~=^" + siteMap.getUrl() + "|^/[a-z]]")){
-                    String cutLink = link.attributes().get("href"); //сокращенная ссылка (без адреса сайта)
-                    String fullLink = cutLink.matches("^" + siteMap.getUrl() + ".*") ? cutLink : siteMap.getUrl() + cutLink; //полная ссылка, вместе с адресом
-                    fullLink += fullLink.matches(".+/$") ? "" : "/"; //добавляем слэш в конец адреса
-                    //если ссылка успешно добавилась в карту сайта, значит это новая ссылка и ее нужно тоже пропарсить
-                    if (siteMap.addChildPage(fullLink)){
-                        System.out.println(Thread.currentThread().getName() + ", найдена новая ссылка: " + fullLink);
-                        mapPageThread(fullLink);
-                    }
-                }
-            } catch (HttpStatusException httpex){
-                System.out.println("Page " + url + " error. " + httpex);
-                //при ошибке когда страница сервера временно не доступна, пытаемся пропарсить ее еще раз
-                if (httpex.getStatusCode() == 503) {
-                    mapPageThread(url);
-                }
-            } catch (IOException iex) {
-                System.out.println("Page " + url + " not found. " + iex.getMessage());
-            }
+        while (countThreads.get() > MAX_THREADS) {
+            Thread.onSpinWait();
+        }
+        new Thread(THREAD_GROUP, () -> {
+            countThreads.incrementAndGet();
+            ArrayList<String> newUrls = parsePage(url);
+            countThreads.decrementAndGet();
+            newUrls.forEach(Main::mapPageThread);
         }).start();
+    }
+
+    /**
+     * @param url - адрес страницы, которую нужно пропарсить на ссылки
+     * @return - возвращает список новых ссылок на страницы, которые уже добавлены в siteMap
+     */
+    public static ArrayList<String> parsePage(String url){
+        ArrayList<String> newUrls = new ArrayList<>();
+        try {
+            Document doc = Jsoup.connect(url).proxy(PROXY).maxBodySize(0).get();
+            //получаем все ссылки которые ссылаются на адрес парсируемого сайта
+            for (Element link : doc.select("a[href~=^" + siteMap.getUrl() + "|^/[a-z]]")){
+                String cutLink = link.attributes().get("href"); //сокращенная ссылка (без адреса сайта)
+                String fullLink = cutLink.matches("^" + siteMap.getUrl() + ".*") ? cutLink : siteMap.getUrl() + cutLink; //полная ссылка, вместе с адресом
+                fullLink += fullLink.matches(".+/$") ? "" : "/"; //добавляем слэш в конец адреса
+                //если ссылка успешно добавилась в карту сайта, значит это новая ссылка и ее нужно тоже пропарсить
+                if (siteMap.addChildPage(fullLink)){
+                    System.out.println(Thread.currentThread().getName() + ", найдена новая ссылка: " + fullLink);
+                    newUrls.add(fullLink);
+                }
+            }
+        } catch (HttpStatusException httpex){
+            System.out.println("Page " + url + " error. " + httpex);
+            //при ошибке когда страница сервера временно не доступна, пытаемся пропарсить ее еще раз
+            if (httpex.getStatusCode() == 503) {
+                newUrls.add(url);
+            }
+        } catch (IOException iex) {
+            System.out.println("Page " + url + " not found. " + iex.getMessage());
+        }
+        return newUrls;
     }
 }
